ad61054d25a7d4894d1e148d8dd1d14f03e61d1f
Ahmet Gökay Arpacı <gokayarpaci@gmail.com>
2022-09-19 00:49:17 +0200

    u

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,18 +1,21 @@
-import { useRef, useCallback } from "react";
+import { useRef, useCallback } from 'react';
 
-export const useDebounce = (cb : (...args: any[]) => void, delay = 2000) => {
-    const timeoutRef = useRef<undefined | NodeJS.Timeout>(undefined);
+export const useDebounce = (cb: (...args: any[]) => void, delay = 2000) => {
+  const timeoutRef = useRef<undefined | NodeJS.Timeout>(undefined);
 
-    const fn = useCallback((...args: any[]) => {
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-        }
+  const fn = useCallback(
+    (...args: any[]) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
 
-        timeoutRef.current = setTimeout(() => {
-            cb(...args);
-            timeoutRef.current = undefined;
-        }, delay);
-    }, [cb, delay]);
+      timeoutRef.current = setTimeout(() => {
+        cb(...args);
+        timeoutRef.current = undefined;
+      }, delay);
+    },
+    [cb, delay]
+  );
 
-    return fn;
+  return fn;
 };
--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,36 +1,39 @@
-import { useRef, useCallback } from "react";
+import { useRef, useCallback } from 'react';
 
-export const useThrottle = (cb: (...args: any[]) => void , delay = 2000) => {
+export const useThrottle = (cb: (...args: any[]) => void, delay = 2000) => {
   const isWaitingRef = useRef(false);
   const paramsRef = useRef<null | any[]>(null);
-  const timeoutRef = useRef<undefined | NodeJS.Timeout>(undefined)
-  
-  const fn = useCallback((...args: any[]) => {
-    const timeoutCb = () => {
-      if(paramsRef.current) {
-        cb(...paramsRef.current);
-        paramsRef.current = null;
+  const timeoutRef = useRef<undefined | NodeJS.Timeout>(undefined);
 
+  const fn = useCallback(
+    (...args: any[]) => {
+      const timeoutCb = () => {
+        if (paramsRef.current) {
+          cb(...paramsRef.current);
+          paramsRef.current = null;
+
+          timeoutRef.current = setTimeout(timeoutCb, delay);
+        } else {
+          isWaitingRef.current = false;
+        }
+      };
+
+      if (isWaitingRef.current) {
+        paramsRef.current = args;
+
+        clearTimeout(timeoutRef.current);
         timeoutRef.current = setTimeout(timeoutCb, delay);
-    } else {
-        isWaitingRef.current = false;
+
+        return;
       }
-    };
 
-    if(isWaitingRef.current) {
-      paramsRef.current = args;
+      cb(...args);
+      isWaitingRef.current = true;
 
-      clearTimeout(timeoutRef.current);
       timeoutRef.current = setTimeout(timeoutCb, delay);
-      
-      return;
-    }
-
-    cb(...args);
-    isWaitingRef.current = true;
-
-    timeoutRef.current = setTimeout(timeoutCb, delay);
-  }, [cb, delay])
+    },
+    [cb, delay]
+  );
 
   return fn;
 };
